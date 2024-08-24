69fdc62b9b8026e29e1cfda86fd688a98f60b71c
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-08-24 12:50:30 +0300

    feat: Add API constants and This commit adds the following changes: - Added `ApiConstants` class with base URL, public key, private key, timestamp, hash, and URL generation function - Added `ApiErrors` class with various error message constants

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,29 @@
+import 'package:marvel_app/core/helpers/converter.dart';
+
+class ApiConstants {
+  static const String apiBaseUrl = 'https://gateway.marvel.com/v1/public/';
+  static const String publicKey = 'becb5f2b050e3df08982f1b3c9320000';
+  static const String privateKey = 'cd1c5f12aa625d1df8eebf3bc3a3f110ca37c577';
+  static int timestamp = DateTime.now().millisecondsSinceEpoch;
+  static String hash = generateMd5('$timestamp$privateKey$publicKey');
+  static String generateUrl(String endpoint) =>
+      '$apiBaseUrl$endpoint?ts=$timestamp&apikey=$publicKey&hash=$hash';
+}
+
+class ApiErrors {
+  static const String badRequestError = "badRequestError";
+  static const String noContent = "noContent";
+  static const String forbiddenError = "forbiddenError";
+  static const String unauthorizedError = "unauthorizedError";
+  static const String notFoundError = "notFoundError";
+  static const String conflictError = "conflictError";
+  static const String internalServerError = "internalServerError";
+  static const String unknownError = "unknownError";
+  static const String timeoutError = "timeoutError";
+  static const String defaultError = "defaultError";
+  static const String cacheError = "cacheError";
+  static const String noInternetError = "noInternetError";
+  static const String loadingMessage = "loading_message";
+  static const String retryAgainMessage = "retry_again_message";
+  static const String ok = "Ok";
+}
